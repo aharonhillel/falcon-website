aea5e798979cca7dd13e6d7c8cc8fae4a2b1f6e0
Aaron Gold <aaron@goldtechsolutions.net>
2025-11-16 16:15:02 +0200

    update to routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import { queryClientInstance } from '@/lib/query-client'
 import VisualEditAgent from '@/lib/VisualEditAgent'
 import NavigationTracker from '@/lib/NavigationTracker'
 import { pagesConfig } from './pages.config'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { createPageUrl } from '@/utils';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
@@ -52,7 +53,7 @@ const AuthenticatedApp = () => {
       {Object.entries(Pages).map(([path, Page]) => (
         <Route
           key={path}
-          path={`/${path}`}
+          path={createPageUrl(path)}
           element={
             <LayoutWrapper currentPageName={path}>
               <Page />
